fb5257b708f0eb16dce6b15bc233f61a6a02b930
Momshad Dinury <md.dinury@gmail.com>
2021-11-02 11:19:41 +0600

    Fix: Keyboard visibility bottom overflow issue

--- listify/lib/views/screens/auth/login_screen.dart
+++ listify/lib/views/screens/auth/login_screen.dart
@@ -15,80 +15,83 @@ class LoginScreen extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: KSize.getWidth(context, 59),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: KSize.getHeight(context, 332)),
-            Container(
-              height: KSize.getHeight(context, 63),
-              width: KSize.getWidth(context, 315),
-              child: Text(
-                "Welcome Back",
-                textAlign: TextAlign.center,
-                style: KTextStyle.headLine3,
-              ),
-            ),
-            SizedBox(height: KSize.getHeight(context, 63)),
-            KTextField(
-              hintText: 'Your email address',
-            ),
-            SizedBox(height: KSize.getHeight(context, 37)),
-            KTextField(
-              hintText: 'Password',
-              isPasswordField: true,
-            ),
-            SizedBox(height: KSize.getHeight(context, 10)),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Text(
-                  'Forgot your password?',
-                  style: KTextStyle.bodyText2().copyWith(fontWeight: FontWeight.normal),
+        child: SingleChildScrollView(
+          physics: BouncingScrollPhysics(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: KSize.getHeight(context, 332)),
+              Container(
+                height: KSize.getHeight(context, 63),
+                width: KSize.getWidth(context, 315),
+                child: Text(
+                  "Welcome Back",
+                  textAlign: TextAlign.center,
+                  style: KTextStyle.headLine3,
                 ),
-              ],
-            ),
-            SizedBox(height: KSize.getHeight(context, 61)),
-            KFilledButton(
-              buttonText: 'Login',
-              onPressed: () {},
-            ),
-            SizedBox(height: KSize.getHeight(context, 66)),
-            Text(
-              "Or",
-              textAlign: TextAlign.center,
-              style: KTextStyle.bodyText1(),
-            ),
-            SizedBox(height: KSize.getHeight(context, 72)),
-            KOutlinedButton(
-              buttonText: 'Login with Google',
-              assetIcon: KAssets.google,
-            ),
-            SizedBox(height: KSize.getHeight(context, 37)),
-            KOutlinedButton(
-              buttonText: 'Login with Facebook',
-              assetIcon: KAssets.facebook,
-            ),
-            SizedBox(height: KSize.getHeight(context, 131)),
-            Text(
-              "Don't have an account?",
-              textAlign: TextAlign.center,
-              style: KTextStyle.bodyText3(),
-            ),
-            SizedBox(height: KSize.getHeight(context, 6)),
-            InkWell(
-              onTap: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(builder: (context) => SignupScreen()),
-                );
-              },
-              child: Text(
-                "Create account",
-                textAlign: TextAlign.center,
-                style: KTextStyle.bodyText2(),
               ),
-            ),
-          ],
+              SizedBox(height: KSize.getHeight(context, 63)),
+              KTextField(
+                hintText: 'Your email address',
+              ),
+              SizedBox(height: KSize.getHeight(context, 37)),
+              KTextField(
+                hintText: 'Password',
+                isPasswordField: true,
+              ),
+              SizedBox(height: KSize.getHeight(context, 10)),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(
+                    'Forgot your password?',
+                    style: KTextStyle.bodyText2().copyWith(fontWeight: FontWeight.normal),
+                  ),
+                ],
+              ),
+              SizedBox(height: KSize.getHeight(context, 61)),
+              KFilledButton(
+                buttonText: 'Login',
+                onPressed: () {},
+              ),
+              SizedBox(height: KSize.getHeight(context, 66)),
+              Text(
+                "Or",
+                textAlign: TextAlign.center,
+                style: KTextStyle.bodyText1(),
+              ),
+              SizedBox(height: KSize.getHeight(context, 72)),
+              KOutlinedButton(
+                buttonText: 'Login with Google',
+                assetIcon: KAssets.google,
+              ),
+              SizedBox(height: KSize.getHeight(context, 37)),
+              KOutlinedButton(
+                buttonText: 'Login with Facebook',
+                assetIcon: KAssets.facebook,
+              ),
+              SizedBox(height: KSize.getHeight(context, 131)),
+              Text(
+                "Don't have an account?",
+                textAlign: TextAlign.center,
+                style: KTextStyle.bodyText3(),
+              ),
+              SizedBox(height: KSize.getHeight(context, 6)),
+              InkWell(
+                onTap: () {
+                  Navigator.pushReplacement(
+                    context,
+                    MaterialPageRoute(builder: (context) => SignupScreen()),
+                  );
+                },
+                child: Text(
+                  "Create account",
+                  textAlign: TextAlign.center,
+                  style: KTextStyle.bodyText2(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
