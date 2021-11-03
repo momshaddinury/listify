54cdc321305fc5c3ca255d42655dab21553f291f
Momshad Dinury <md.dinury@gmail.com>
2021-11-03 12:48:42 +0600

    #1 iOS integration done

---
+++ android/app/src/main/kotlin/com/listify/listify/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.listify.listify
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- android/app/src/main/kotlin/com/listify/listify_app/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.listify.listify_app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
