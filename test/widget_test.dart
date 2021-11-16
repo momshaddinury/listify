c95c6db6dbd0c29a1eaa8e4366cdc8b5ce201701
Momshad Dinury <md.dinury@gmail.com>
2021-11-16 12:17:15 +0600

    Counter example test commented out

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -11,7 +11,7 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:listify/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  /* testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
     await tester.pumpWidget(MyApp());
 
@@ -26,5 +26,5 @@ void main() {
     // Verify that our counter has incremented.
     expect(find.text('0'), findsNothing);
     expect(find.text('1'), findsOneWidget);
-  });
+  }); */
 }
