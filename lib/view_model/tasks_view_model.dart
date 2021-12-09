8be63fdd61af8266a1de6c933956f148fa774385
Momshad Dinury <md.dinury@gmail.com>
2021-12-09 11:39:12 +0600

    Update tasks_view_model.dart

--- lib/view_model/tasks_view_model.dart
+++ lib/view_model/tasks_view_model.dart
@@ -12,15 +12,15 @@ class TasksViewModel extends GetxController {
 
   CollectionReference get userTasksCollection => tasksCollection.doc(box.read(USER_UID)).collection('usertasks');
 
-  Future createNewTask(String title, description, dateTime, priority) => _tasksRepository.createNewTask(title, description, dateTime, priority);
+  Future<void> createNewTask(String title, description, dateTime, priority) => _tasksRepository.createNewTask(title, description, dateTime, priority);
 
-  Future updateTask(uid, title, description, dateTime, priority) => _tasksRepository.updateTask(uid, title, description, dateTime, priority);
+  Future<void> updateTask(uid, title, description, dateTime, priority) => _tasksRepository.updateTask(uid, title, description, dateTime, priority);
 
-  Future completeTask(uid) => _tasksRepository.completeTask(uid);
+  Future<void> completeTask(uid) => _tasksRepository.completeTask(uid);
 
-  Future undoCompleteTask(uid) => _tasksRepository.undoCompleteTask(uid);
+  Future<void> undoCompleteTask(uid) => _tasksRepository.undoCompleteTask(uid);
 
-  Future removeTodo(uid) => _tasksRepository.removeTodo(uid);
+  Future<void> removeTodo(uid) => _tasksRepository.removeTodo(uid);
 
   Stream<List<Todo>> pendingTasks() => _tasksRepository.pendingTasks();
 
