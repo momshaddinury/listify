3b616ea260508b737f91f6ea8d777f8c15484cce
Momshad Dinury <md.dinury@gmail.com>
2021-12-16 02:03:36 +0600

    Update welcome_screen.dart

--- lib/views/screens/startup/welcome_screen.dart
+++ lib/views/screens/startup/welcome_screen.dart
@@ -16,57 +16,59 @@ class WelcomeScreen extends StatelessWidget {
         padding: EdgeInsets.symmetric(
           horizontal: KSize.getWidth(59),
         ),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(height: KSize.getHeight(288)),
-            Container(
-              width: KSize.getWidth(439),
-              child: Text(
-                "Start Using Listify App Today!",
-                textAlign: TextAlign.center,
-                style: KTextStyle.headLine3,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: KSize.getHeight(288)),
+              Container(
+                width: KSize.getWidth(439),
+                child: Text(
+                  "Start Using Listify App Today!",
+                  textAlign: TextAlign.center,
+                  style: KTextStyle.headLine3,
+                ),
               ),
-            ),
-            SizedBox(height: KSize.getHeight(61)),
-            KFilledButton(
-              buttonText: 'Create Account',
-              onPressed: () => SignupScreen().push(context),
-            ),
-            SizedBox(height: KSize.getHeight(106)),
-            Text(
-              "Or",
-              textAlign: TextAlign.center,
-              style: KTextStyle.bodyText1(),
-            ),
-            SizedBox(height: KSize.getHeight(87)),
-            KOutlinedButton(
-              buttonText: 'Sign up with Google',
-              assetIcon: KAssets.google,
-            ),
-            SizedBox(height: KSize.getHeight(37)),
-            KOutlinedButton(
-              buttonText: 'Sign up with Facebook',
-              assetIcon: KAssets.facebook,
-            ),
-            SizedBox(height: KSize.getHeight(308)),
-            Text(
-              "Already have an account?",
-              textAlign: TextAlign.center,
-              style: KTextStyle.bodyText3(),
-            ),
-            SizedBox(height: KSize.getHeight(6)),
-            InkWell(
-              onTap: () {
-                LoginScreen().push(context);
-              },
-              child: Text(
-                "Login",
-                textAlign: TextAlign.center,
-                style: KTextStyle.bodyText2(),
+              SizedBox(height: KSize.getHeight(61)),
+              KFilledButton(
+                buttonText: 'Create Account',
+                onPressed: () => SignupScreen().push(context),
               ),
-            ),
-          ],
+              SizedBox(height: KSize.getHeight(106)),
+              Text(
+                "Or",
+                textAlign: TextAlign.center,
+                style: KTextStyle.bodyText1(),
+              ),
+              SizedBox(height: KSize.getHeight(87)),
+              KOutlinedButton(
+                buttonText: 'Sign up with Google',
+                assetIcon: KAssets.google,
+              ),
+              SizedBox(height: KSize.getHeight(37)),
+              KOutlinedButton(
+                buttonText: 'Sign up with Facebook',
+                assetIcon: KAssets.facebook,
+              ),
+              SizedBox(height: KSize.getHeight(308)),
+              Text(
+                "Already have an account?",
+                textAlign: TextAlign.center,
+                style: KTextStyle.bodyText3(),
+              ),
+              SizedBox(height: KSize.getHeight(6)),
+              InkWell(
+                onTap: () {
+                  LoginScreen().push(context);
+                },
+                child: Text(
+                  "Login",
+                  textAlign: TextAlign.center,
+                  style: KTextStyle.bodyText2(),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
