8b76d5982a3e17375c5ae55899d691d9c97aa758
Momshad Dinury <md.dinury@gmail.com>
2022-01-25 16:27:03 +0600

    Update welcome_screen.dart

--- lib/views/screens/startup/welcome_screen.dart
+++ lib/views/screens/startup/welcome_screen.dart
@@ -14,66 +14,53 @@ class WelcomeScreen extends KBaseScreen {
 
 class _WelcomeScreenState extends KBaseState<WelcomeScreen> {
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.symmetric(horizontal: 25),
-        child: SingleChildScrollView(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: KSize.getHeight(288)),
-              Container(
-                width: KSize.getWidth(439),
-                child: Text(
-                  "Start Using Listify App Today!",
-                  textAlign: TextAlign.center,
-                  style: KTextStyle.headLine3,
-                ),
-              ),
-              SizedBox(height: KSize.getHeight(61)),
-              KFilledButton(
-                buttonText: 'Create Account',
-                onPressed: () => SignupScreen().push(context),
-              ),
-              SizedBox(height: KSize.getHeight(106)),
-              Text(
-                "Or",
-                textAlign: TextAlign.center,
-                style: KTextStyle.bodyText1(),
-              ),
-              SizedBox(height: KSize.getHeight(87)),
-              KOutlinedButton.iconText(
-                buttonText: 'Sign up with Google',
-                assetIcon: KAssets.google,
-              ),
-              SizedBox(height: KSize.getHeight(37)),
-              KOutlinedButton.iconText(
-                buttonText: 'Sign up with Facebook',
-                assetIcon: KAssets.facebook,
-              ),
-              SizedBox(height: KSize.getHeight(308)),
-              Text(
-                "Already have an account?",
-                textAlign: TextAlign.center,
-                style: KTextStyle.bodyText3(),
-              ),
-              SizedBox(height: KSize.getHeight(6)),
-              KTextButton(
-                  buttonText: "Login",
-                  onPressed: () {
-                    LoginScreen().push(context);
-                  })
-            ],
+  Widget body() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        SizedBox(height: KSize.getHeight(288)),
+        Container(
+          width: KSize.getWidth(439),
+          child: Text(
+            "Start Using Listify App Today!",
+            textAlign: TextAlign.center,
+            style: KTextStyle.headLine3,
           ),
         ),
-      ),
+        SizedBox(height: KSize.getHeight(61)),
+        KFilledButton(
+          buttonText: 'Create Account',
+          onPressed: () => SignupScreen().push(context),
+        ),
+        SizedBox(height: KSize.getHeight(106)),
+        Text(
+          "Or",
+          textAlign: TextAlign.center,
+          style: KTextStyle.bodyText1(),
+        ),
+        SizedBox(height: KSize.getHeight(87)),
+        KOutlinedButton.iconText(
+          buttonText: 'Sign up with Google',
+          assetIcon: KAssets.google,
+        ),
+        SizedBox(height: KSize.getHeight(37)),
+        KOutlinedButton.iconText(
+          buttonText: 'Sign up with Facebook',
+          assetIcon: KAssets.facebook,
+        ),
+        SizedBox(height: KSize.getHeight(308)),
+        Text(
+          "Already have an account?",
+          textAlign: TextAlign.center,
+          style: KTextStyle.bodyText3(),
+        ),
+        SizedBox(height: KSize.getHeight(6)),
+        KTextButton(
+            buttonText: "Login",
+            onPressed: () {
+              LoginScreen().push(context);
+            })
+      ],
     );
-  }
-
-  @override
-  Widget body() {
-    // TODO: implement body
-    throw UnimplementedError();
   }
 }
