7ae8ca1d755508dee913e8cf093490f3e92543f3
Momshad Dinury <md.dinury@gmail.com>
2022-03-20 00:29:18 +0600

    code cleaned and weird git bug solved

--- lib/views/screens/auth/auth_wrapper.dart
+++ lib/views/screens/auth/auth_wrapper.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:listify/controller/authentication/authentication_provider.dart';
-import 'package:listify/services/network_status.dart';
 import 'package:listify/views/screens/home_screen.dart';
 import 'package:listify/views/screens/k_base_screen.dart';
 import 'package:listify/views/screens/startup/welcome_screen.dart';
@@ -22,24 +21,12 @@ class _AuthenticationWrapperState extends KBaseState<AuthenticationWrapper> {
 
   @override
   Widget body() {
-<<<<<<< Updated upstream
     final asyncUser = ref.watch(authStateChangesProvider);
 
     return asyncUser.when(
-      data: (user) {
-        return user != null ? HomeScreen() : WelcomeScreen();
-      },
+      data: (user) => user != null ? HomeScreen() : WelcomeScreen(),
       loading: () => WelcomeScreen(),
       error: (e, stackTrace) => ErrorScreen(),
     );
-=======
-    return ref.watch(authStateChangesProvider).when(
-          data: (user) {
-            return user != null ? HomeScreen() : WelcomeScreen();
-          },
-          loading: () => WelcomeScreen(),
-          error: (e, stackTrace) => ErrorScreen(),
-        );
->>>>>>> Stashed changes
   }
 }
--- lib/views/screens/k_base_screen.dart
+++ lib/views/screens/k_base_screen.dart
@@ -1,47 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:listify/services/network_status.dart';
-<<<<<<< Updated upstream
-
 import 'error_screen.dart';
-import 'startup/welcome_screen.dart';
-=======
 import 'package:listify/views/screens/error_screen.dart';
-import 'package:listify/views/screens/startup/welcome_screen.dart';
->>>>>>> Stashed changes
 
 abstract class _BaseView {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: appBar(),
       body: Consumer(builder: (context, ref, _) {
-<<<<<<< Updated upstream
-        final asyncNetworkStatus = ref.watch(networkStatusProvider);
-
-        return asyncNetworkStatus.when(
-          data: (result) {
-            return scrollable()
-                ? SingleChildScrollView(
-                    physics: BouncingScrollPhysics(),
-                    child: Padding(
-                      padding: EdgeInsets.symmetric(
-                          horizontal: defaultPadding() ? 25 : 0),
-                      child: result == NetworkStatus.ONLINE
-                          ? body()
-                          : ErrorScreen(),
-                    ),
-                  )
-                : Padding(
-                    padding: EdgeInsets.symmetric(
-                        horizontal: defaultPadding() ? 25 : 0),
-                    child:
-                        result == NetworkStatus.ONLINE ? body() : ErrorScreen(),
-                  );
-          },
-          loading: () => WelcomeScreen(),
-          error: (e, stackTrace) => ErrorScreen(),
-        );
-=======
         return ref.watch(networkStatusProvider).when(
             data: (network) {
               return network == NetworkStatus.OFFLINE
@@ -61,7 +28,6 @@ abstract class _BaseView {
             },
             loading: () => CircularProgressIndicator.adaptive(),
             error: (e, stackTrace) => KErrorWidget());
->>>>>>> Stashed changes
       }),
     );
   }
